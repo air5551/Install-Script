e0bc6a81b840123a270bdbca0277c19eb2201bf8
Air5551 <zacupham102@gmail.com>
2024-09-29 19:10:27 -0400

    Cleaned up the installation Script

--- Install.sh
+++ Install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [ "$(whoami)" == "root" ]; 
+if [ "$(whoami)" == "root" ];
 then
 echo Root check passed!
 curl -fsSL https://rpm.librewolf.net/librewolf-repo.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
-dnf install librewolf -y 
+dnf install librewolf -y
+echo installing RPM's
+curl -Lo GoogleChrome.rpm https://www.google.com/chrome/next-steps.html?platform=linux&statcb=0&installdataindex=empty&defaultbrowser=0#
+curl -Lo vencord.rpm https://vencord.dev/download/vesktop/amd64/rpm
+dnf install vencord.rpm -y
+dnf install GoogleChrome.rpm -y
 echo installing flatpaks
-flatpak install flathub --system org.libreoffice.LibreOffice -y 
-flatpak install flathub --system org.kde.audiotube -y 
+flatpak install flathub --system org.libreoffice.LibreOffice -y
+flatpak install flathub --system org.kde.audiotube -y
 flatpak install flathub --system org.mozilla.Thunderbird -y
 flatpak install flathub --system io.missioncenter.MissionCenter -y
 echo installing QT Creator
 curl -Lo QTCreator.run https://d13lb3tujbc8s0.cloudfront.net/onlineinstallers/qt-online-installer-linux-x64-4.8.1.run
 ./QTCreator.run
-echo installing RPM's 
-curl -Lo GoogleChrome.rpm https://www.google.com/chrome/next-steps.html?platform=linux&statcb=0&installdataindex=empty&defaultbrowser=0#
-curl -Lo vencord.rpm https://vencord.dev/download/vesktop/amd64/rpm
-dnf install vencord.rpm -y 
-dnf install GoogleChrome.rpm -y 
 echo Installing Zed
 curl -Lo zed.tar.gz https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
 tar -xvf zed.tar.gz -C ~/.local
