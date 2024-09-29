45e227f2f4f0a00a352649ea2918fbd6510100a8
Air5551 <zacupham102@gmail.com>
2024-09-29 19:35:22 -0400

    Script Madness

--- Install.sh
+++ Install.sh
@@ -3,6 +3,7 @@
 if [ "$(whoami)" == "root" ];
 then
 echo Root check passed!
+cd ~/Downloads/
 curl -fsSL https://rpm.librewolf.net/librewolf-repo.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
 dnf install librewolf -y
 echo installing RPM's
@@ -15,13 +16,13 @@ flatpak install flathub --system org.libreoffice.LibreOffice -y
 flatpak install flathub --system org.kde.audiotube -y
 flatpak install flathub --system org.mozilla.Thunderbird -y
 flatpak install flathub --system io.missioncenter.MissionCenter -y
-echo installing QT Creator
-curl -Lo QTCreator.run https://d13lb3tujbc8s0.cloudfront.net/onlineinstallers/qt-online-installer-linux-x64-4.8.1.run
-./QTCreator.run
-echo Installing Zed
+echo installing Zed
 curl -Lo zed.tar.gz https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
 tar -xvf zed.tar.gz -C ~/.local
 ln -sf ~/.local/zed.app/bin/zed ~/.local/bin/zed
 cp ~/.local/zed.app/share/applications/zed.desktop ~/.local/share/applications/dev.zed.Zed.desktop
 sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" ~/.local/share/applications/dev.zed.Zed.desktop
 sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/libexec/zed-editor|g" ~/.local/share/applications/dev.zed.Zed.desktop
+echo installing QT Creator
+curl -Lo QTCreator.run https://d13lb3tujbc8s0.cloudfront.net/onlineinstallers/qt-online-installer-linux-x64-4.8.1.run
+./QTCreator.run
