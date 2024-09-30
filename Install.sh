2941c4a4282396a9ca4a860ac578de644f04267f
air5551 <166467006+air5551@users.noreply.github.com>
2024-09-30 16:37:51 -0400

    Added the $user tag on testing the root functions

--- Install.sh
+++ Install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+if ["$(whoami)" == "root"]
+user = "Username here" 
+then 
 echo Root check passed Make sure this is sudo
-cd ~/Downloads/
+cd $user/Downloads/
 curl -fsSL https://rpm.librewolf.net/librewolf-repo.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
 dnf install librewolf -y
 echo installing RPMs
@@ -16,11 +18,12 @@ flatpak install flathub --system org.mozilla.Thunderbird -y
 flatpak install flathub --system io.missioncenter.MissionCenter -y
 echo installing Zed
 curl -Lo zed.tar.gz https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
-tar -xvf zed.tar.gz -C ~/.local
-ln -sf ~/.local/zed.app/bin/zed ~/.local/bin/zed
-cp ~/.local/zed.app/share/applications/zed.desktop ~/.local/share/applications/dev.zed.Zed.desktop
-sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" ~/.local/share/applications/dev.zed.Zed.desktop
-sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/libexec/zed-editor|g" ~/.local/share/applications/dev.zed.Zed.desktop
+tar -xvf zed.tar.gz -C $user/.local
+ln -sf $user/.local/zed.app/bin/zed $user/.local/bin/zed
+cp $user/.local/zed.app/share/applications/zed.desktop $user/.local/share/applications/dev.zed.Zed.desktop
+sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" $user/.local/share/applications/dev.zed.Zed.desktop
+sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/libexec/zed-editor|g" $user/.local/share/applications/dev.zed.Zed.desktop
 echo installing QT Creator
 curl -Lo QTCreator.run https://d13lb3tujbc8s0.cloudfront.net/onlineinstallers/qt-online-installer-linux-x64-4.8.1.run
 ./QTCreator.run
+fi 
