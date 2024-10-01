9f4b938f6bd99f3e772bdb738d4d915bafb6c4ad
air5551 <166467006+air5551@users.noreply.github.com>
2024-10-01 10:20:55 -0400

    After some testing, I added the enable line for flathub

--- Install.sh
+++ Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ["$(whoami)" == "root"]
-user = "Username here" 
+user = Username here 
 then 
 echo Root check passed Make sure this is sudo
 cd $user/Downloads/
@@ -11,18 +11,19 @@ curl -Lo GoogleChrome.rpm https://www.google.com/chrome/next-steps.html?platform
 curl -Lo vencord.rpm https://vencord.dev/download/vesktop/amd64/rpm
 dnf install vencord.rpm -y
 dnf install GoogleChrome.rpm -y
-echo installing flatpaks
-flatpak install flathub --system org.libreoffice.LibreOffice -y
+echo installing flatpaks and enabling the flathub repo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub --system org.libreoffice.LibreOffice -y | echo Check if flathub is enabled
 flatpak install flathub --system org.kde.audiotube -y
 flatpak install flathub --system org.mozilla.Thunderbird -y
 flatpak install flathub --system io.missioncenter.MissionCenter -y
 echo installing Zed
 curl -Lo zed.tar.gz https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
-tar -xvf zed.tar.gz -C $user/.local
-ln -sf $user/.local/zed.app/bin/zed $user/.local/bin/zed
-cp $user/.local/zed.app/share/applications/zed.desktop $user/.local/share/applications/dev.zed.Zed.desktop
-sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" $user/.local/share/applications/dev.zed.Zed.desktop
-sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/libexec/zed-editor|g" $user/.local/share/applications/dev.zed.Zed.desktop
+tar -xvf zed.tar.gz -C /home/$user/.local
+ln -sf /home/$user/.local/zed.app/bin/zed /home/$user/.local/bin/zed
+cp /home/$user/.local/zed.app/share/applications/zed.desktop /home/$user/.local/share/applications/dev.zed.Zed.desktop
+sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" /home/$user/.local/share/applications/dev.zed.Zed.desktop
+sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/libexec/zed-editor|g" /home/$user/.local/share/applications/dev.zed.Zed.desktop
 echo installing QT Creator
 curl -Lo QTCreator.run https://d13lb3tujbc8s0.cloudfront.net/onlineinstallers/qt-online-installer-linux-x64-4.8.1.run
 ./QTCreator.run
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Paste this command in terminal
 
 bash (curl -fsS https://raw.githubusercontent.com/air5551/Install-Script/refs/heads/main/Install.sh)
+This commonly fails under root
+
+# Offline mode
+
+curl -Lo Install.sh https://raw.githubusercontent.com/air5551/Install-Script/refs/heads/main/Install.sh
+chmod +x Install.sh
+bash Install.sh
